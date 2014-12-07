01a414c9d3f0d26abec227fb6d53d4b94809905e
qrafts <elmar@qrafts.nl>
2014-12-07 16:38:11 +0100

    First commit

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Oostvaarders
+//
+//  Created by Elmar Haneveld on 07/12/14.
+//  Copyright (c) 2014 Elmar Haneveld. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Oostvaarders
+//
+//  Created by Elmar Haneveld on 07/12/14.
+//  Copyright (c) 2014 Elmar Haneveld. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
